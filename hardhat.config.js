7b4e8a3f1c650dda66783ec07e5b334f9fbbeb8b
Ross Campbell <rcampbell_sec@protonmail.com>
2023-02-20 12:18:59 +0000

    added tasks

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,18 +1,23 @@
-require("@nomicfoundation/hardhat-toolbox");
-require("dotenv").config();
+require("@nomicfoundation/hardhat-toolbox")
+require("dotenv").config()
+require("./tasks/block-number.js")
 
-const RINKEBY_RPC_URL = process.env.RINKEBY_RPC_URL;
-const RINKEBY_PRIVATE_KEY = process.env.RINKEBY_PRIVATE_KEY;
+const GOERLI_RPC_URL = process.env.GOERLI_RPC_URL
+const GOERLI_PRIVATE_KEY = process.env.GOERLI_PRIVATE_KEY
+const ETHERSCAN_API_KEY = process.env.ETHERSCAN_API_KEY
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
-  defaultNetwork: "hardhat",
-  networks: {
-    rinkeby: {
-      url: RINKEBY_RPC_URL,
-      accounts: [RINKEBY_PRIVATE_KEY],
-      chainId: 4,
-    },
-  },
-  solidity: "0.8.17",
-};
+	defaultNetwork: "hardhat",
+	networks: {
+		goerli: {
+			url: GOERLI_RPC_URL,
+			accounts: [GOERLI_PRIVATE_KEY],
+			chainId: 5,
+		},
+	},
+	solidity: "0.8.17",
+	etherscan: {
+		apiKey: ETHERSCAN_API_KEY,
+	},
+}
--- scripts/deploy.js
+++ scripts/deploy.js
@@ -1,14 +1,54 @@
-const { ethers } = require("hardhat") // we can also require it in the old way
+const { ethers, run, network } = require("hardhat") // we can also require it in the old way
+require("dotenv").config()
 
 async function main() {
 	const contract8 = await ethers.getContractFactory("Contract8mytoken")
 
 	const myContract = await contract8.deploy("RossToken", "RT")
 	await myContract.deployed()
-	console.log(`deployed to: ${myContract.address}`)
+	console.log(`Deployed To: ${myContract.address}`)
+
+	if (network.config.chainId === 5 && process.env.ETHERSCAN_API_KEY) {
+		// best practice to wait for a couple of blocks to be mined before verifying, else it won't be ready
+		await myContract.deployTransaction.wait(6) // wait 1 minute on ETH?
+		await verify(myContract.address, ["RossToken", "RT"])
+	}
+
+	await myContract.mint()
+
+	// a very crude way to iterate over the publicly accessible array in the contract
+	let x = 0
+	var addr
+	while (true) {
+		try {
+			addr = await myContract.owners(x)
+		} catch (e) {
+			break
+		}
+		console.log(addr)
+		x++
+	}
+	//_owners.forEach((addr) => console.log(addr))
+	//console.log(`first owner is: ${await myContract.owners(0)}`)
 }
 
-async function verify_deployment(contractAddress, args) {}
+async function verify(_contractAddress, _args) {
+	console.log("Verifying contract...")
+	try {
+		await run("verify:verify", {
+			// this is the command line command from hardhat-etherscan
+			address: _contractAddress,
+			constructorArguments: _args,
+		})
+	} catch (e) {
+		if (e.message.toLowerCase().includes("already verified")) {
+			// we want it to work if already verified, instead of halting execution
+			console.log("already verified!")
+		} else {
+			console.log(e)
+		}
+	}
+}
 
 main()
 	.then(() => process.exit())
